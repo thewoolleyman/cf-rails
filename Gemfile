c25017217ec9edf0666c9444fb7f553a7cd6c8e8
Chad Woolley <thewoolleyman@gmail.com>
2013-11-27 11:00:17 -0700

    setting up rails project...

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source "https://rubygems.org"
+
+gem "bundler", "1.3.5"
+gem "rake", "0.9.2.2"
+gem "rails", "2.3.18"
+gem "mysql", "2.8.1"
+
+group :development, :test do
+  gem "rspec", "1.3.1"
+  gem "rspec-rails", "1.3.2"
+end
---
+++ config/environment.rb
@@ -0,0 +1,23 @@
+dir = File.dirname(__FILE__)
+
+require File.join(File.dirname(__FILE__), 'boot')
+POST_LOAD_BLOCKS = [] unless Object.const_defined?("POST_LOAD_BLOCKS")
+
+Rails::Initializer.run do |config|
+  ::ActiveSupport::Deprecation.silenced = true
+  config.frameworks -= [:action_web_service]
+
+  config.active_record.pluralize_table_names = false
+  config.active_record.default_timezone = :utc
+  config.action_controller.perform_caching = false
+  config.active_record.observers = []
+  config.time_zone = "UTC"
+  config.action_controller.session = { :key => "t_session", :secret => "so secret even I don't know it"}
+  config.action_controller.ip_spoofing_check = false
+
+  config.after_initialize do
+    ActionController::Base.session_options[:secure] = t2config(:use_ssl)
+  end
+end
+
+POST_LOAD_BLOCKS.each { |proc| proc.call }
